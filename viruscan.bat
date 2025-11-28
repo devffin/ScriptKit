30206bd34b72614dad9db621502336f13f0035a3
devffin <lessupersenfants.theo@gmail.com>
2025-11-28 19:15:56 +0100

    Create viruscan.bat for antivirus updates and scans

---
+++ viruscan.bat
@@ -0,0 +1,15 @@
+@echo off
+title devffin ScriptKit v1
+
+echo ===============================
+echo     ViruScan - ScriptKit v1
+echo ===============================
+echo.
+echo Updating the antivirus ...
+PowerShell -Command "Update-MpSignature"
+echo Scanning ...
+PowerShell -Command "Start-MpScan -ScanType FullScan"
+echo Finished!
+PowerShell -Command "Get-MpThreatDetection"
+
+pause
